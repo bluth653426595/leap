5a13612b1dcf5a01c91dc8a3c41ccd6014a59319
bluth653426595 <ycl_pj@163.com>
2013-09-03 22:24:03 +0800

    update Makefile

---
+++ LeapSDK/note.java
@@ -0,0 +1,50 @@
+c Attributes
+            STATE_INVALID =(-1)
+        An invalid state.
+         
+            STATE_START =(1)
+        The gesture is starting.
+         
+            STATE_STOP =(3)
+        The gesture has completed or stopped.
+         
+            STATE_UPDATE =(2)
+        The gesture is in progress.
+
+
+gesture type
+gc Attributes
+            TYPE_CIRCLE =(4)
+        A circular movement by a finger.
+         
+            TYPE_INVALID =(-1)
+        An invalid type.
+         
+            TYPE_KEY_TAP =(6)
+        A downward tapping movement by a finger.
+         
+            TYPE_SCREEN_TAP =(5)
+        A forward tapping movement by a finger.
+         
+            TYPE_SWIPE =(1)
+        A straight line movement by the hand with fingers extended.gesture 
+
+
+ValueType
+
+Vc Attributes
+            TYPE_BOOLEAN =(1)
+        A boolean value.
+         
+            TYPE_FLOAT =(6)
+        A floating-point number.
+         
+            TYPE_INT32 =(2)
+        A 32-bit integer.
+         
+            TYPE_STRING =(8)
+        A string of characters.
+         
+            TYPE_UNKNOWN =(0)
+        The data type is unknown. ValueType
+
