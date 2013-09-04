edd8816f7b0c920ce00043b9be896cf3446a4697
bluth653426595 <ycl_pj@163.com>
2013-09-04 16:17:41 +0800

    add dbus and test ok

--- LeapSDK/samples/Sample.cpp
+++ LeapSDK/samples/Sample.cpp
@@ -8,6 +8,9 @@
 
 #include <iostream>
 #include "Leap.h"
+#include "cmath"
+#include <dbus/dbus.h>
+using namespace std;
 using namespace Leap;
 
 class SampleListener : public Listener {
@@ -19,6 +22,7 @@ class SampleListener : public Listener {
     virtual void onFrame(const Controller&);
     virtual void onFocusGained(const Controller&);
     virtual void onFocusLost(const Controller&);
+    void emit_dbus_signal(const char *signame );
 };
 
 void SampleListener::onInit(const Controller& controller) {
@@ -45,56 +49,33 @@ void SampleListener::onExit(const Controller& controller) {
 void SampleListener::onFrame(const Controller& controller) {
   // Get the most recent frame and report some basic information
   const Frame frame = controller.frame();
-/*  std::cout << "Frame id: " << frame.id()*/
-            //<< ", timestamp: " << frame.timestamp()
-            //<< ", hands: " << frame.hands().count()
-            //<< ", fingers: " << frame.fingers().count()
-            //<< ", tools: " << frame.tools().count()
-            //<< ", gestures: " << frame.gestures().count() << std::endl;
+  // std::cout << ", hands: " << frame.hands().count()
+  //           << ", fingers: " << frame.fingers().count()
+  //           << ", gestures: " << frame.gestures().count() << std::endl;
 
- if (!frame.hands().empty()) {
-    std::cout <<"hands isnt empty";
-    // Get the first hand
-    const Hand hand = frame.hands()[0];
 
-    // Check if the hand has any fingers
-    const FingerList fingers = hand.fingers();
-    if (!fingers.empty()) {
-      // Calculate the hand's average finger tip position
-      std::cout <<"fingers isnt empty";
-      Vector avgPos;
-      for (int i = 0; i < fingers.count(); ++i) {
-        avgPos += fingers[i].tipPosition();
-      }
-      avgPos /= (float)fingers.count();
-      std::cout << "Hand has " << fingers.count();
-                //<< " fingers, average finger tip position" << avgPos << std::endl;
-    }
+    int LEAP_INVALIABLE = -1;
+    int LIFE_HIDE = 0;//hand_to_left
+    int LIFE_SHOW = 1;//hand_to_right
+    int FINGER_CIRCLE = 2;
+    int FINGER_TO_LEFT = 3;
+    int FINGER_TO_RIGHT = 4;
+    int FINGER_TO_UP = 5;
+    int FINGER_TO_DOWN = 6;
+    int FINGER_PRESS = 7;
 
-    // Get the hand's sphere radius and palm position
-    //std::cout << "Hand sphere radius: " << hand.sphereRadius()
-              //<< " mm, palm position: " << hand.palmPosition() << std::endl;
 
-    // Get the hand's normal vector and direction
-    const Vector normal = hand.palmNormal();
-    const Vector direction = hand.direction();
+    int result = -2;
 
-    // Calculate the hand's pitch, roll, and yaw angles
-   /* std::cout << "Hand pitch: " << direction.pitch() * RAD_TO_DEG << " degrees, "*/
-              //<< "roll: " << normal.roll() * RAD_TO_DEG << " degrees, "
-              /*<< "yaw: " << direction.yaw() * RAD_TO_DEG << " degrees" << std::endl;*/
-  }
-
-  // Get gestures
+ // Get gestures
   const GestureList gestures = frame.gestures();
-  std::cout <<",gestures.count():"<<gestures.count();
   for (int g = 0; g < gestures.count(); ++g) {
     Gesture gesture = gestures[g];
 
     switch (gesture.type()) {
       case Gesture::TYPE_CIRCLE:
       {
-        std::cout <<"circle";
+        // std::cout <<"circle----------";
         CircleGesture circle = gesture;
         std::string clockwiseness;
 
@@ -110,46 +91,101 @@ void SampleListener::onFrame(const Controller& controller) {
           CircleGesture previousUpdate = CircleGesture(controller.frame(1).gesture(circle.id()));
           sweptAngle = (circle.progress() - previousUpdate.progress()) * 2 * PI;
         }
-        /*std::cout << "Circle id: " << gesture.id()*/
-                  //<< ", state: " << gesture.state()
-                  //<< ", progress: " << circle.progress()
-                  //<< ", radius: " << circle.radius()
-                  //<< ", angle " << sweptAngle * RAD_TO_DEG
-                  /*<<  ", " << clockwiseness << std::endl;*/
+        // float angle = sweptAngle * RAD_TO_DEG;
+        // std::cout << "angle: " << angle << std::endl;
+
+        if(frame.fingers().count() == 1)
+        {
+          // std::cout << "fingers circle " << std::endl;
+          result = FINGER_CIRCLE;
+
+        }
+
         break;
       }
       case Gesture::TYPE_SWIPE:
       {
-        std::cout <<"swipe";
+        // std::cout <<"swipe,";
         SwipeGesture swipe = gesture;
-        /*std::cout << "Swipe id: " << gesture.id()*/
-          //<< ", state: " << gesture.state()
-          std::cout << ", direction: " << swipe.direction();
-          /*<< ", speed: " << swipe.speed() << std::endl;*/
+        // std::cout << "startPosition:" << swipe.startPosition() << std::endl;
+        // std::cout << "currentPosition:" << swipe.position() << std::endl;
+
+        float start_x = swipe.startPosition().x;
+        float current_x = swipe.position().x;
+
+        float start_y = swipe.startPosition().y;
+        float current_y = swipe.position().y;
+
+        int fingers_count = frame.fingers().count();
+        int gestures_count = frame.gestures().count();
+        int hands_count = frame.hands().count();
+
+        if( hands_count > -1 && fingers_count == 1 && gestures_count == 1 )
+        {
+          // std::cout << "fingers------" ;
+          if((start_x - current_x) > 100 && std::abs(start_y - current_y) < 100)
+          {
+            // std::cout << "finger to left " << std::endl;
+            result = FINGER_TO_LEFT;
+            emit_dbus_signal("FINGER_TO_LEFT");
+
+          }
+          else if ((start_x - current_x) < -100 && std::abs(start_y - current_y) < 100){
+            // std::cout << "finger to right " << std::endl;
+            result = FINGER_TO_RIGHT;
+            emit_dbus_signal("FINGER_TO_RIGHT");
+          }
+
+          if((start_y - current_y) < -100)
+          {
+            // std::cout << "finger to up " << std::endl;
+            result = FINGER_TO_UP;
+            emit_dbus_signal("FINGER_TO_UP");
+          }
+          else if ((start_y - current_y) > 100){
+            // std::cout << "finger to down " << std::endl;
+            result = FINGER_TO_DOWN;
+            emit_dbus_signal("FINGER_TO_DOWN");
+          }
+
+        }
+        else if (hands_count > 0 && fingers_count > 1 && gestures_count > 1)
+        {
+          // std::cout << "hand-------" ;
+          if((start_x - current_x) > 100 && std::abs(start_y - current_y) < 100)
+          {
+            // std::cout << "hand to left " << std::endl;
+            result = LIFE_HIDE;
+            emit_dbus_signal("LIFE_HIDE");
+          }
+          else if ((start_x - current_x) < -100 && std::abs(start_y - current_y) < 100){
+            // std::cout << "hand to right " << std::endl;
+            result = LIFE_SHOW;
+            emit_dbus_signal("LIFE_SHOW");
+          }
+
+        }
+
         break;
       }
       case Gesture::TYPE_KEY_TAP:
       {
-        std::cout <<"key_tap";
+        // std::cout <<"key_tap";
         KeyTapGesture tap = gesture;
-       /* std::cout << "Key Tap id: " << gesture.id()*/
-          //<< ", state: " << gesture.state()
-          //<< ", position: " << tap.position()
-          std::cout << ", direction: " << tap.direction()<< std::endl;
+        result = FINGER_PRESS;
+        emit_dbus_signal("FINGER_PRESS");
         break;
       }
       case Gesture::TYPE_SCREEN_TAP:
       {
         std::cout <<"screen_tap";
         ScreenTapGesture screentap = gesture;
-        /*std::cout << "Screen Tap id: " << gesture.id()*/
-        //<< ", state: " << gesture.state()
-        //<< ", position: " << screentap.position()
-        std::cout<< ", direction: " << screentap.direction()<< std::endl;
         break;
       }
       default:
         std::cout << "Unknown gesture type." << std::endl;
+        result = LEAP_INVALIABLE;
+        // emit_dbus_signal("LEAP_INVALIABLE");
         break;
     }
   }
@@ -157,6 +193,8 @@ void SampleListener::onFrame(const Controller& controller) {
   if (!frame.hands().empty() || !gestures.empty()) {
     std::cout << std::endl;
   }
+
+  
 }
 
 void SampleListener::onFocusGained(const Controller& controller) {
@@ -183,4 +221,44 @@ int main() {
   controller.removeListener(listener);
 
   return 0;
+}
+
+void SampleListener::emit_dbus_signal(const char *signame )
+{
+    DBusConnection *dbus_conn = NULL;
+    DBusError dbus_err;
+    DBusMessage *msg = NULL;
+    // DBusMessageIter args;
+
+    dbus_error_init(&dbus_err);
+    dbus_conn = dbus_bus_get(DBUS_BUS_SESSION, &dbus_err);
+
+    if ( dbus_error_is_set(&dbus_err) ) {
+        // sys_err("get conn error : %s\n", dbus_err.message);
+        dbus_error_free(&dbus_err);
+    }
+
+    if ( dbus_conn == NULL ) {
+        // sys_err("conn NULL...\n");
+        return ;
+    }
+
+    msg = dbus_message_new_signal ("/com/deepn/lsearch", 
+      "com.deepn.lsearch", signame);
+
+    if ( msg == NULL ) {
+        // sys_err("msg NULL...\n");
+        return ;
+    }
+
+    //send message
+    if ( !dbus_connection_send(dbus_conn, msg, NULL) ) {
+        // sys_err("Out Of Memory!\n");
+        return ;
+    }
+
+    dbus_connection_flush(dbus_conn);
+    cout<< signame<<" Signal has been sent..."<<endl;
+
+    dbus_message_unref(msg);
 }
