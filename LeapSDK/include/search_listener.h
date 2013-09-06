b93706bad08d20137393b7c470727164641c2380
kosl90 <kos1990l@gmail.com>
2013-09-06 15:23:32 +0800

    format and remove suprlus codes

--- LeapSDK/include/search_listener.h
+++ LeapSDK/include/search_listener.h
@@ -15,7 +15,7 @@ public:
     virtual void onFrame(const Leap::Controller&);
     virtual void onFocusGained(const Leap::Controller&);
     virtual void onFocusLost(const Leap::Controller&);
-    void emit_dbus_signal(const std::string signame);
+    void emit_dbus_signal(const std::string& signame);
 };
 
 #endif /* end of include guard: _SEARCH_LISENTER_H_ */
--- LeapSDK/src/search_listener.cpp
+++ LeapSDK/src/search_listener.cpp
@@ -6,36 +6,44 @@
 using namespace std;
 using namespace Leap;
 
-void SearchListener::onInit(const Controller& controller) {
+void SearchListener::onInit(const Controller& controller)
+{
     cout << "Initialized" << endl;
 }
 
-void SearchListener::onConnect(const Controller& controller) {
+void SearchListener::onConnect(const Controller& controller)
+{
     cout << "Connected" << endl;
     controller.enableGesture(Gesture::TYPE_CIRCLE);
     controller.enableGesture(Gesture::TYPE_KEY_TAP);
-    controller.enableGesture(Gesture::TYPE_SCREEN_TAP);
     controller.enableGesture(Gesture::TYPE_SWIPE);
 }
 
-void SearchListener::onDisconnect(const Controller& controller) {
+void SearchListener::onDisconnect(const Controller& controller)
+{
     //Note: not dispatched when running in a debugger.
     cout << "Disconnected" << endl;
 }
 
-void SearchListener::onExit(const Controller& controller) {
+void SearchListener::onExit(const Controller& controller)
+{
     cout << "Exited" << endl;
 }
 
-void SearchListener::onFrame(const Controller& controller) {
+void SearchListener::onFrame(const Controller& controller)
+{
     // Get the most recent frame and report some basic information
     const Frame frame = controller.frame();
+
+    if (frame.hands().empty())
+        return;
+
     // cout << ", hands: " << frame.hands().count()
     //           << ", fingers: " << frame.fingers().count()
     //           << ", gestures: " << frame.gestures().count() << endl;
 
 
-    /*    int LIFE_HIDE = 0;//hand_to_left*/
+    //int LIFE_HIDE = 0;//hand_to_left
     //int LIFE_SHOW = 1;//hand_to_right
     //int FINGER_CIRCLE = 2;
     //int FINGER_TO_LEFT = 3;
@@ -50,91 +58,67 @@ void SearchListener::onFrame(const Controller& controller) {
 
     // Get gestures
     const GestureList gestures = frame.gestures();
+
     for (int g = 0; g < gestures.count(); ++g) {
         Gesture gesture = gestures[g];
 
         switch (gesture.type()) {
-        case Gesture::TYPE_CIRCLE:
-            {
-                CircleGesture circle = gesture;
-                string clockwiseness;
+        case Gesture::TYPE_CIRCLE: {
+            CircleGesture circle = gesture;
 
-                if (circle.pointable().direction().angleTo(circle.normal()) <= PI/4) {
-                    clockwiseness = "clockwise";
-                } else {
-                    clockwiseness = "counterclockwise";
-                }
-
-                if(frame.fingers().count() == 1)
-                {
-                    emit_dbus_signal("FingerCircle");
-                }
-
-                break;
+            if (frame.fingers().count() == 1
+                && gesture.state() == Gesture::STATE_STOP) {
+                emit_dbus_signal("FingerCircle");
             }
-        case Gesture::TYPE_SWIPE:
-            {
-                SwipeGesture swipe = gesture;
-                // cout << "startPosition:" << swipe.startPosition() << endl;
-                // cout << "currentPosition:" << swipe.position() << endl;
 
-                float start_x = swipe.startPosition().x;
-                float current_x = swipe.position().x;
+            break;
+        }
+        case Gesture::TYPE_SWIPE: {
+            SwipeGesture swipe = gesture;
 
-                float start_y = swipe.startPosition().y;
-                float current_y = swipe.position().y;
+            Vector start = swipe.startPosition();
+            Vector current = swipe.position();
 
-                int fingers_count = frame.fingers().count();
-                int gestures_count = frame.gestures().count();
-                int hands_count = frame.hands().count();
+            int fingers_count = frame.fingers().count();
+            int gestures_count = frame.gestures().count();
+            int hands_count = frame.hands().count();
 
-                if( hands_count > -1 && fingers_count == 1 && gestures_count == 1 )
-                {
-                    if((start_x - current_x) > 100 && abs(start_y - current_y) < 100)
-                    {
-                        emit_dbus_signal("FingerLeft");
+            if (fingers_count == 1 && gestures_count == 1) {
+                if ((start.x - current.x) > 100
+                    && abs(start.y - current.y) < 100) {
+                    emit_dbus_signal("FingerLeft");
 
-                    }
-                    else if ((start_x - current_x) < -100 && abs(start_y - current_y) < 100){
-                        emit_dbus_signal("FingerRight");
-                    }
-
-                    if((start_y - current_y) < -300)
-                    {
-                        emit_dbus_signal("FingerUp");
-                    }
-                    else if ((start_y - current_y) > 300){
-                        emit_dbus_signal("FingerDown");
-                    }
+                } else if ((start.x - current.x) < -100
+                           && abs(start.y - current.y) < 100) {
+                    emit_dbus_signal("FingerRight");
                 }
-                else if (hands_count > 0 && fingers_count > 1 && gestures_count > 1)
-                {
-                    if((start_x - current_x) > 100 && abs(start_y - current_y) < 100)
-                    {
-                        emit_dbus_signal("HandLeft");
-                    }
-                    else if ((start_x - current_x) < -100 && abs(start_y - current_y) < 100){
-                        emit_dbus_signal("HandRight");
-                    }
+
+                if ((start.y - current.y) < -300) {
+                    emit_dbus_signal("FingerUp");
+                } else if ((start.y - current.y) > 300) {
+                    emit_dbus_signal("FingerDown");
                 }
-                break;
-            }
-        case Gesture::TYPE_KEY_TAP:
-            {
-                KeyTapGesture tap = gesture;
-                int fingers_count = frame.fingers().count();
-                if(fingers_count == 1)
-                {
-                    emit_dbus_signal("FingerClick");
+            } else if (fingers_count > 1 && gestures_count > 1
+                       && abs(start.y - current.y) < 100) {
+                if ((start.x - current.x) > 100) {
+                    emit_dbus_signal("HandLeft");
+                } else if ((start.x - current.x) < -100) {
+                    emit_dbus_signal("HandRight");
                 }
-                break;
             }
-        case Gesture::TYPE_SCREEN_TAP:
-            {
-                cout <<"screen_tap";
-                ScreenTapGesture screentap = gesture;
-                break;
+
+            break;
+        }
+        case Gesture::TYPE_KEY_TAP: {
+            KeyTapGesture tap = gesture;
+            int fingers_count = frame.fingers().count();
+
+            if (fingers_count == 1) {
+                emit_dbus_signal("FingerClick");
             }
+
+            break;
+        }
         default:
             cout << "Unknown gesture type." << endl;
             break;
@@ -147,50 +131,52 @@ void SearchListener::onFrame(const Controller& controller) {
 
 }
 
-void SearchListener::onFocusGained(const Controller& controller) {
+void SearchListener::onFocusGained(const Controller& controller)
+{
     cout << "Focus Gained" << endl;
 }
 
-void SearchListener::onFocusLost(const Controller& controller) {
+void SearchListener::onFocusLost(const Controller& controller)
+{
     cout << "Focus Lost" << endl;
 }
 
 
-void SearchListener::emit_dbus_signal(const string signame )
+void SearchListener::emit_dbus_signal(const string& signame)
 {
-    DBusConnection *dbus_conn = NULL;
+    DBusConnection* dbus_conn = NULL;
     DBusError dbus_err;
-    DBusMessage *msg = NULL;
+    DBusMessage* msg = NULL;
 
     dbus_error_init(&dbus_err);
     dbus_conn = dbus_bus_get(DBUS_BUS_SESSION, &dbus_err);
 
-    if ( dbus_error_is_set(&dbus_err) ) {
-        cout<<"get conn error : "<<dbus_err.message<<endl;
+    if (dbus_error_is_set(&dbus_err)) {
+        cout << "get conn error : " << dbus_err.message << endl;
         dbus_error_free(&dbus_err);
     }
 
-    if ( dbus_conn == NULL ) {
-        cout<<"conn NULL..."<<endl;
+    if (dbus_conn == NULL) {
+        cout << "conn NULL..." << endl;
         return ;
     }
 
-    msg = dbus_message_new_signal ("/com/deepin/lsearch",
-                                   "com.deepin.lsearch", signame.c_str());
+    msg = dbus_message_new_signal("/com/deepin/lsearch",
+                                  "com.deepin.lsearch", signame.c_str());
 
-    if ( msg == NULL ) {
-        cout<<"msg NULL..."<<endl;
+    if (msg == NULL) {
+        cout << "msg NULL..." << endl;
         return ;
     }
 
     //send message
-    if ( !dbus_connection_send(dbus_conn, msg, NULL) ) {
-        cout<<"Out of Memory !"<<endl;
+    if (!dbus_connection_send(dbus_conn, msg, NULL)) {
+        cout << "Out of Memory !" << endl;
         return ;
     }
 
     dbus_connection_flush(dbus_conn);
-    cout<< signame<<" Signal has been sent..."<<endl;
+    cout << signame << " Signal has been sent..." << endl;
 
     dbus_message_unref(msg);
 }
